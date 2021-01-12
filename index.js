432d1da93f098e04fb0fa5418363353a9a3cc6e8
ocawthorne <ocawthorne@gmail.com>
2021-01-12 01:20:52 +0000

    fetch-lab

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  //   return response.json();
+  // }) .then(function(json) {
+  //   renderBooks(json)
+  // });
+    .then(func => func.json())
+    .then(json => renderBooks(json))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main')
